b5c7a1958060b73b39d9822ac2eb76e8768dc162
Patrick Diego <paah-diego@hotmail.com>
2021-04-19 14:30:06 -0300

    fixing env

---
+++ devquiz/lib/core/app_widget.dart
@@ -0,0 +1,22 @@
+// import 'package:DevQuiz/home/home_page.dart';
+// import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("DEVQUIZ"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        body: Text("Olá usuário"),
+      ),
+    );
+  }
+}
